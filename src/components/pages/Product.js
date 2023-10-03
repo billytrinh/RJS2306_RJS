9e5d0fbfcad7f1f3b70ccf52155dba3ee22c5195
quanghoatrinh <quanghoa.trinh@gmail.com>
2023-10-03 21:02:29 +0700

    action reducer

--- src/components/pages/Product.js
+++ src/components/pages/Product.js
@@ -2,6 +2,7 @@ import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import api from "../../api";
 import Context from "../../context/context";
+import ACTION from "../../context/action";
 // Global state: state dành cho tòan bộ web, tức là component nào cũng có thể sử dụng
 function Product(){
     const {id} = useParams();
@@ -27,7 +28,7 @@ function Product(){
         const cart = state.cart;
         cart.push(product);
         // setState({...state,cart:cart});
-        dispatch({type:"UPDATE_CART",payload:cart});
+        dispatch({type:ACTION.UPDATE_CART,payload:cart});
 
     }
     return (
--- src/context/reducer.js
+++ src/context/reducer.js
@@ -1,9 +1,9 @@
-
+import ACTION from "./action";
 const reducer = (state,action) =>{ 
     // giả định rằng trong action sẽ có 2 thông tin là type(Kiểu tác động) và payload (dữ liệu)
     switch(action.type){
-        case "UPDATE_CART": return {...state,cart:action.payload};
-        case "UPDATE_FAVORITES": return {...state,favorites:action.payload};
+        case ACTION.UPDATE_CART: return {...state,cart:action.payload};
+        case ACTION.UPDATE_FAVORITES: return {...state,favorites:action.payload};
     }
 }
 export default reducer;
