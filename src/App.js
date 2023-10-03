5e97820f569afcbf4c63d3ef13f2e69d63cb34a1
quanghoatrinh <quanghoa.trinh@gmail.com>
2023-10-03 20:02:24 +0700

    add to cart

--- src/App.js
+++ src/App.js
@@ -5,10 +5,14 @@ import Home from "./components/pages/Home";
 import Category from "./components/pages/Category";
 import Product from "./components/pages/Product";
 import Weather from "./components/pages/Weather";
+import { AppProvider } from "./context/context";
+import { useState } from "react";
+import STATE from "./context/initState";
 
 function App() { // jsx
-  
+  const [state,setState] = useState(STATE)
   return (
+    <AppProvider value={{state,setState}}>
       <div className="app">
         <Header />
         <Menu/>
@@ -22,6 +26,7 @@ function App() { // jsx
            
         </main>
       </div>
+      </AppProvider>
   );
 }
 
--- src/context/context.js
+++ src/context/context.js
@@ -4,3 +4,7 @@ const Context = React.createContext(); // tạo 1 biến chứa giá trị của
 
 export const AppProvider = Context.Provider;
 export default Context;
+
+// Global state
+// context : có sẵn trong React
+// redux: là 1 package bên ngoài, cần cài đặt và độc lập với react
